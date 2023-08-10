03e663a0d8be045f0b200e34afba307e0f03a05e
devansh.jain <devansh.jain@G1-BWXRM13-L>
2023-08-10 23:10:38 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    internal class Class1
+    {
+        public string? myLocalVar;
+        public void sharpen()
+        {
+            if(myLocalVar != null)
+            {
+                myLocalVar = "Devansh";
+            }
+        }
+
+        public bool MakePencil(int l1)
+        {
+            if (l1 > 10)
+                return true;
+            else
+                return false;
+        }
+
+
+        public bool checkDateTime()
+        {
+            DateTime dt = new DateTime(2023,8,10);
+            if (dt.Day >= 10)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using FirstConsole;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Class1 c = new Class1();
+        c.myLocalVar = "New Var";
+        c.sharpen();
+        Console.WriteLine(c.myLocalVar);
+        Console.WriteLine(c.MakePencil(c.myLocalVar.Length));
+    }
+}
